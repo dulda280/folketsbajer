18ed8fe5050a15a88cc6345d81e952afcef06cde
Thomas S.T. Andersen <tsta19@student.aau.dk>
2022-04-16 16:41:58 +0200

    Updated navbar on all pages

--- php/login.php
+++ php/login.php
@@ -92,14 +92,55 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Folkets Bajer: Login</title>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/toppen_style.css">
     <style>
-        body{ font: 14px sans-serif; }
         .wrapper{ width: 360px; padding: 20px; }
     </style>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #2D348A">
+    <div class="container-fluid">
+        <a class="navbar-brand" style="color: white" href="#">Folkets Bajer</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" style="color: white" href="../html/index.html">Forside</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white" href="../html/toppen.html">Toppen</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" style="color: white" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Øl Kategorier
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="../html/kategori_a.html">Bedste Pilsner</a></li>
+                        <li><a class="dropdown-item" href="#">Bedste Festivalsbajer</a></li>
+                        <li><a class="dropdown-item" href="#">Bedste xyz</a></li>
+                        <li><a class="dropdown-item" href="#">Bedste xyz</a></li>
+                        <li><a class="dropdown-item" href="#">Bedste xyz</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white" href="../html/om_os.html">Om Os</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link button white-color btn btn-outline-primary" href="../php/register.php">Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link button white-color btn btn-outline-primary active" href="../php/login.php">Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <div class="wrapper">
     <h2>Login</h2>
     <p>Please fill in your credentials to login.</p>
--- php/welcome.php
+++ php/welcome.php
@@ -8,3 +8,56 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Folkets Bajer: Profil</title>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/toppen_style.css">
+    <style>
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #2D348A">
+    <div class="container-fluid">
+        <a class="navbar-brand" style="color: white" href="#">Folkets Bajer</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" style="color: white" href="../html/index.html">Forside</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white" href="../html/toppen.html">Toppen</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" style="color: white" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Øl Kategorier
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="../html/kategori_a.html">Bedste Pilsner</a></li>
+                        <li><a class="dropdown-item" href="#">Bedste Festivalsbajer</a></li>
+                        <li><a class="dropdown-item" href="#">Bedste xyz</a></li>
+                        <li><a class="dropdown-item" href="#">Bedste xyz</a></li>
+                        <li><a class="dropdown-item" href="#">Bedste xyz</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white" href="../html/om_os.html">Om Os</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" style="color: white" href="#">Profile: Name of Profile</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+</body>
+</html>
