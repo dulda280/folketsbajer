6d85823fed7f6f5c0ef75564495cbeb480efe375
Thomas S.T. Andersen <tsta19@student.aau.dk>
2022-04-17 21:52:53 +0200

    Updated changes

--- page-predefines/navbar.php
+++ page-predefines/navbar.php
@@ -11,7 +11,7 @@ $current_page = PageName();
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #2D348A">
     <div class="container-fluid">
         <a class="navbar-brand" style="color: white" href="../php/index.php">Folkets Bajer</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler white-color" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
