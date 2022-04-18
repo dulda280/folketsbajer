14102e56cdfa586a51c0921f7bcd7b92e1e0b8d6
Thomas S.T. Andersen <tsta19@student.aau.dk>
2022-04-18 16:27:22 +0200

    Toppen looking dope

--- php/toppen.php
+++ php/toppen.php
@@ -18,61 +18,124 @@
 
 <?php include_once("../page-predefines/navbar.php");?>
 
-<!-- Top Beer Container -->
-<div class="top-beer-container">
-    <div class="div-generator">
-        <div class="beer-card-index">
-            <div class="card text-center">
-                <div class="beer-card-title">
-                    <h2>BEER TITLE</h2>
+<section class="card-container">
+    <article class="card">
+        <h3 class="card__title center">Tuborg Classic</h3>
+        <br>
+        <div class="image-container">
+            <img class="card_thumbnail" src="../pics/tbclas.jpg">
+        </div>
+        <br>
+        <main class="card__description">
+            <p class="beer_description center">Dette er en meget fin tuborg classic i en endnu finere grøn dåse</p>
+        </main>
+        <br>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front hvr-shutter-in-vertical">
+                    <h1 id="sum-number-index">3.8<img src="../pics/star-filled.png" style="width: 15%;height: 15%;margin-bottom: .3em;margin-left: .2em" class="rating-star" alt=""></h1>
                 </div>
-                <div class="image-div text-center">
-                    <img class="card-img-top" src="../pics/ceresTOP.jpeg" alt="Card image cap">
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">Tuborg Classic</h5>
-                    <p class="card-text">Kort introduktion.</p>
-
-                    <div class="flip-card">
-                        <div class="flip-card-inner">
-                            <div class="flip-card-front hvr-shutter-in-vertical">
-                                <h1 id="sum-number-index">3.8<img src="../pics/star-filled.png" style="width: 15%;height: 15%;margin-bottom: .3em;margin-left: .2em" class="rating-star" alt=""></h1>
-                            </div>
-                            <div class="flip-card-back">
-                                <div class="rating-container">
-                                    <div class="rating-score">
-                                        <h5 class="rating-title" id="rating-title-x">Smag</h5>
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-unfilled.png" class="rating-star" alt="">
-                                    </div>
-                                    <div class="rating-score">
-                                        <h5 class="rating-title" id="rating-title-y">Chuckability</h5>
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                    </div>
-                                    <div class="rating-score">
-                                        <h5 class="rating-title" id="rating-title-z">Design</h5>
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-filled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-unfilled.png" class="rating-star" alt="">
-                                        <img src="../pics/star-unfilled.png" class="rating-star" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div class="flip-card-back">
+                    <div class="rating-score">
+                        <h5 class="rating-title" id="rating-title-x">Smag</h5>
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-unfilled.png" class="rating-star" alt="">
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </article>
+    <article class="card">
+        <h3 class="card__title center">Tuborg Classic</h3>
+        <br>
+        <div class="image-container">
+            <img class="card_thumbnail" src="../pics/tbclas.jpg">
+        </div>
+        <br>
+        <main class="card__description">
+            <p class="beer_description center">Dette er en meget fin tuborg classic i en endnu finere grøn dåse</p>
+        </main>
+        <br>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front hvr-shutter-in-vertical">
+                    <h1 id="sum-number-index">3.8<img src="../pics/star-filled.png" style="width: 15%;height: 15%;margin-bottom: .3em;margin-left: .2em" class="rating-star" alt=""></h1>
+                </div>
+                <div class="flip-card-back">
+                    <div class="rating-score">
+                        <h5 class="rating-title" id="rating-title-x">Smag</h5>
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-unfilled.png" class="rating-star" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+    <article class="card">
+        <h3 class="card__title center">Tuborg Classic</h3>
+        <br>
+        <div class="image-container">
+            <img class="card_thumbnail" src="../pics/tbclas.jpg">
+        </div>
+        <br>
+        <main class="card__description">
+            <p class="beer_description center">Dette er en meget fin tuborg classic i en endnu finere grøn dåse</p>
+        </main>
+        <br>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front hvr-shutter-in-vertical">
+                    <h1 id="sum-number-index">3.8<img src="../pics/star-filled.png" style="width: 15%;height: 15%;margin-bottom: .3em;margin-left: .2em" class="rating-star" alt=""></h1>
+                </div>
+                <div class="flip-card-back">
+                    <div class="rating-score">
+                        <h5 class="rating-title" id="rating-title-x">Smag</h5>
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-unfilled.png" class="rating-star" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+    <article class="card">
+        <h3 class="card__title center">Tuborg Classic</h3>
+        <br>
+        <div class="image-container">
+            <img class="card_thumbnail" src="../pics/tbclas.jpg">
+        </div>
+        <br>
+        <main class="card__description">
+            <p class="beer_description center">Dette er en meget fin tuborg classic i en endnu finere grøn dåse</p>
+        </main>
+        <br>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front hvr-shutter-in-vertical">
+                    <h1 id="sum-number-index">3.8<img src="../pics/star-filled.png" style="width: 15%;height: 15%;margin-bottom: .3em;margin-left: .2em" class="rating-star" alt=""></h1>
+                </div>
+                <div class="flip-card-back">
+                    <div class="rating-score">
+                        <h5 class="rating-title" id="rating-title-x">Smag</h5>
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-filled.png" class="rating-star" alt="">
+                        <img src="../pics/star-unfilled.png" class="rating-star" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="../js/flip-card-onclick.js"></script>
